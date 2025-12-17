15cd3f82f74a0125746171f8de1b8da4ad2a7598
lakshya1333 <lakshyajain906@gmail.com>
2025-12-17 21:14:52 +0530

    Done writing the schemas will be using Mongodb as i am storing nodes

---
+++ packages/db/index.ts
@@ -0,0 +1,140 @@
+import mongoose,{mongo, Schema} from "mongoose";
+
+const UserSchema = new Schema({
+    username : {
+        type: String,
+        required: true
+    },
+    password : {
+        type: String,
+        required: true
+    }
+})
+
+const EdgesSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    source: {
+        type: String,
+        required: true
+    },
+    target: {
+        type: String,
+        required: true
+    }
+},{
+    _id: false
+})
+
+const PositionSchema = new Schema({
+    x: {
+        type: Number,
+        required: true
+    },
+    y: {
+        type: Number,
+        required: true
+    }
+},{
+    _id: false
+})
+
+const NodeDataSchema = new Schema({
+    kind: {
+        type: String,
+        enum: ["ACTION","TRIGGER"]
+    },
+    metadata: Schema.Types.Mixed
+},{
+    _id: false
+})
+
+
+const WorkflowNodesSchema = new Schema({
+    id:{
+        type: String,
+        required: true
+    },
+    position: PositionSchema,
+    credentials: Schema.Types.Mixed,
+    nodeId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Nodes'
+    },
+    data: NodeDataSchema
+},{
+    _id: false
+})
+
+const WorkflowSchema = new Schema({
+    userId : {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: 'Users' 
+    },
+    nodes: [WorkflowNodesSchema],
+    edges: [EdgesSchema]
+})
+
+const CredentialsTypeSchema = new Schema({
+    title : {
+        type: String,
+        required: true
+    },
+    // type : {
+    //     type: String,
+    //     required: true,
+    //     enum: ["string","number"]
+    // },
+    required : {
+        type: Boolean,
+        required: true
+    },
+},{
+    _id: false
+})
+
+const NodesSchema = new Schema({
+    title : {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+        required: true
+    },
+    type : {
+        type: String,
+        enum: ["ACTION","TRIGGER"],
+        required: true
+    },
+    credentialsType : [CredentialsTypeSchema]
+})
+
+const ExecutionSchema = new Schema({
+    workflowId: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: 'Workflows'
+    },
+    status : {
+        type: String,
+        enum: ["PENDING","SUCCESS","FAILURE"]
+    },
+    startTime: {
+        type: Date,
+        default: Date.now(),
+        required: true
+    },
+    endtime: {
+        type: Date
+    }
+})
+
+
+export const UserModel = mongoose.model("Users",UserSchema);
+export const WorkflowModel = mongoose.model("Workflows",WorkflowSchema);
+export const NodesModel = mongoose.model("Nodes",NodesSchema)
+export const ExecutionModel = mongoose.model("Executions",ExecutionSchema)
